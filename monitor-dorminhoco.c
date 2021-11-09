5a38a9d2c7301267eff75fcceb260349020f6334
jorgebatista <jorgebatistafilho@gmail.com>
2021-11-09 15:09:24 -0300

    Add files via upload

--- monitor-dorminhoco.c
+++ monitor-dorminhoco.c
@@ -7,12 +7,11 @@
 
 #define NUM_ALUNOS 100 // Número de alunos que irão procurar atendimento de monitoria
 
-// vagas: semaforo que registra as vagas (número de cadeiras de espera + cadeira da sala do mobitor)
-// controle_fila: mutex para evitar condições de corrida ao gerenciar o semaforo de vagas
-// monitor: semaforo para controlar o estado do monitor (dormindo/acordado)
-sem_t vagas, controle_fila, monitor;
-int cadeiras; // Cadeiras de espera
-
+// fila: semaforo que registra quantos alunos estão aguardando atendimento.
+// controle: mutex para evitar condições de corrida ao gerenciar o semaforo de fila e o contador de vagas
+// monitoria: semaforo para um aluno aguardar a final;ização do seu atendimento
+sem_t fila, controle, monitoria;
+int cadeiras, vagas;
 
 
 int nanosleep(const struct timespec *req, struct timespec *rem);
@@ -20,71 +19,52 @@ int nanosleep(const struct timespec *req, struct timespec *rem);
 // Função para suspender execução, em milisegundos
 int mssleep(long miliseconds)
 {
-   struct timespec rem;
-   struct timespec req= {
-       (int)(miliseconds / 1000),
-       (miliseconds % 1000) * 1000000
-   };
+  struct timespec rem;
+  struct timespec req = {
+    (int)(miliseconds / 1000),
+    (miliseconds % 1000) * 1000000
+  };
 
-   return nanosleep(&req , &rem);
+  return nanosleep(&req , &rem);
 }
 
 
 // Código da thread do monitor
 void *threadMonitor () {
-  int sval;
-
   while(1) {
-    sem_wait(&monitor); // Espera ser acordado
-    sem_wait(&controle_fila);
-    sem_getvalue(&vagas, &sval); // Checa o tamanho da fila
-    sem_post(&controle_fila);
-
-    // Repete o loop até liberar todas as vagas
-    while(sval <= cadeiras) {
-      sem_wait(&controle_fila);
-      printf("Um aluno foi atendido.\n"); // Atende um aluno
-      sem_post(&vagas); // Libera uma vaga
-      sem_getvalue(&vagas, &sval);
-      // Dorme se todas as vagas estiverem liberadas
-      if(sval == cadeiras+1) {
-        printf("O monitor dormiu.\n");
-      }
-      sem_post(&controle_fila);
-      mssleep(rand() % 2000);
-    }
+    sem_wait(&fila); // Espera ter alguém na fila
+    mssleep(rand() % 1000);
+    sem_wait(&controle); // Espera pelo controle
+    printf("O monitor atendeu um aluno.\n");
+    sem_post(&monitoria); // Realiza a monitoria
+    vagas++; // Libera uma vaga
+    sem_post(&controle); // Libera o controle
   }
 }
 
 
 // Código das threads dos alunos
 void *threadAluno () {
-  int sval, na_fila = 0;
-
-  // Executa o loop enquanto o aluno não entrar na fila
-  while(na_fila == 0) {
-    mssleep(rand() % (NUM_ALUNOS * 1000)); // Espera aleatória
-    sem_wait(&controle_fila);
-    sem_getvalue(&vagas, &sval);
-    // Checa se tem vaga na fila
-    if(sval > 0) {
-      // Se a fila estiver vazia acorda o monitor
-      if(sval == cadeiras + 1) {
-        printf("Um aluno acordou o monitor\n");
-        sem_post(&monitor);
-      }
-      // Se a fila não está vazia mas tem vaga, entra na fila
-      else {
-        printf("Um aluno entrou na fila.\n");
-      }
-      sem_wait(&vagas);
-      na_fila = 1;
+  mssleep(rand() % 60000); // Espera um tempo aleatório antes de ir para a monitoria
+  sem_wait(&controle); // Espera pelo controle
+  // Testa se há vagas
+  if(vagas > 0) {
+    vagas--;
+    sem_post(&fila); // Entra na fila
+    // Checa se o monitor está dormindo (se ele é o único aluno na fila)
+    if(vagas == cadeiras) {
+      printf("Um aluno chegou e acordou o monitor.\n");
     }
-    // Se não tiver vaga repete o loop
     else {
-      printf("Um aluno chegou e encontrou a fila cheia. Ele retornará mais tarde.\n");
+      printf("Um aluno chegou e entrou na fila.\n");
     }
-    sem_post(&controle_fila);
+    sem_post(&controle); // Libera controle
+
+    sem_wait(&monitoria); // Aguarda a finalizaçào do atendimento
+  }
+  else {
+    printf("Um aluno chegou e não encontrou vagas. Ele retornará em outro horário.\n");
+    sem_post(&controle); // Libera o controle
   }
 
   pthread_exit(NULL);
@@ -100,18 +80,19 @@ int main(int argc, char *argv[]) {
   }
 
   char *end;
-  // Testa se o argumento é um inteiro positivo
+  // Testa se o argumento é um inteiro
   cadeiras = strtol(argv[1], &end, 10);
   if(cadeiras < 0 || *end != 0) {
-    printf("\nERRO - O programa deve receber um único inteiro positivo como argumento. (número de cadeiras na sala de espera)\n\n");
+    printf("\nERRO - O programa deve receber um único inteiro maior ou igual a 0 como argumento. (número de cadeiras na sala de espera)\n\n");
     exit(1);
   }
 
-  // Inicializa os semáforos
-  sem_init(&vagas, 0, cadeiras+1); // Cadeiras de espera + cadeira da sala do monitor
-  sem_init(&controle_fila, 0, 1);
-  sem_init(&monitor, 0, 0);
+  vagas = cadeiras + 1;
 
+  // Inicializa os semáforos
+  sem_init(&monitoria, 0, 0);
+  sem_init(&fila, 0, 0);
+  sem_init(&controle, 0, 1);
 
   pthread_t monitor, alunos[NUM_ALUNOS];
 
